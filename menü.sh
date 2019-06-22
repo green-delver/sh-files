f9547f7aee02a7e25681b16166469597a077157d
green-delver <52094613+green-delver@users.noreply.github.com>
2019-06-22 17:09:04 +0200

    Create menü.sh

---
+++ menü.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "1: x"
+echo "2: y"
+
+read ANTWORT
+
+if [ "$ANTWORT" == "1" ] 
+	then
+		clear
+		#Befehl
+    
+    #
+		exit
+fi
+
+if [ "$ANTWORT" == "2" ] 
+	then
+		clear
+	  #Befehl
+    
+    #
+		exit
+fi
+
+echo "Tastatureingabe nicht im erlaubten Bereich!"
